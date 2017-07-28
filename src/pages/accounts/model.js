2cf4ee41b2f96e4b995ef37ee60ac96cb97ab312
Sam Grundman <samuel@yodas.ws>
2017-07-28 12:29:23 -0400

    List Accounts

--- src/pages/accounts/model.js
+++ src/pages/accounts/model.js
@@ -4,8 +4,14 @@ angular.module('pageAccounts')
 .controller('ctrlAccounts', ($scope) => {
 	$scope.accounts = [
 		{
-			id: '0173',
-			balance: 6808,
+			id: '0001',
+			name: 'Bill Pay',
+			balance: 1200,
+		},
+		{
+			id: '0002',
+			name: 'Everyday',
+			balance: 808,
 		},
 	]
 })
--- src/pages/accounts/routes.js
+++ src/pages/accounts/routes.js
@@ -3,9 +3,11 @@
 angular.module('pageAccounts')
 .config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
 	$routeProvider.when('/account/:id', {
-		templateUrl: 'pages/accounts/accounts.html',
+		templateUrl: 'pages/accounts/account.html',
 	})
 	$routeProvider.when('/accounts', {
 		templateUrl: 'pages/accounts/accounts.html',
+		controller: 'ctrlAccounts',
+		constrollerAs: '$ctrl',
 	})
 }])
