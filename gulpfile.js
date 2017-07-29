bf4c7b6c22da011bde92142dad3f6a9801c2273a
Sam Grundman <samuel@yodas.ws>
2017-07-29 18:43:57 -0400

    Remove duplicate lint options object

--- gulpfile.js
+++ gulpfile.js
@@ -248,44 +248,6 @@ options = {
 
 		}
 	},
-	lintHTML:{
-		useHtmllintrc: false,
-		rules: {
-
-'attr-name-style': 'dash',
-'attr-no-dup': true,
-'attr-req-value': false,
-'class-name-style': 'dash',
-'class-no-dup': true,
-'doctype-html5': true,
-'fig-req-figcaption': false,
-'head-req-title': true,
-'head-valid-content-model': true,
-'html-req-lang': true,
-'id-class-style': 'dash',
-'id-no-dup': true,
-'img-req-alt': true,
-'img-req-src': true,
-'indent-style': 'tabs',
-'indent-width-cont': true,
-'input-radio-req-name': true,
-'input-req-label': true,
-'label-req-for': true,
-'line-end-style': 'lf',
-'table-req-caption': false,
-'table-req-header': false, // this is buggy in htmllint (https://github.com/htmllint/htmllint/issues/197)
-'tag-bans': [
-	'acronym','applet','basefont','big','blink','center','font','frame','frameset','isindex','noframes','marquee',
-	'style',
-],
-'tag-close': true,
-'tag-name-lowercase': true,
-'tag-name-match': true,
-'tag-self-close': 'always',
-'title-no-dup': true,
-
-		}
-	},
 	prefixCSS:{
 		// more options at https://github.com/postcss/autoprefixer#options
 		browsers: [
